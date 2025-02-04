9ee0a9c22694ae3051f22eebe703f1fff57a36c5
AbabilGroup <itek.department@gmail.com>
2025-02-04 17:56:41 +0600

    add social links on mobile navigation drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,12 @@ import Link from "next/link";
 import { FaBars } from "react-icons/fa6";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { useState } from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaPinterestP } from "react-icons/fa";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -44,29 +50,81 @@ const Header = () => {
               <FaBars className="cursor-pointer text-3xl" />
             </SheetTrigger>
             <SheetContent>
-              <Link href="/">
-                <Image
-                  className="h-auto w-[100px]"
-                  src="/logo.png"
-                  alt="Bridge The Gap Logo"
-                  height={65}
-                  width={150}
-                />
-              </Link>
+              <div className="flex min-h-screen flex-col">
+                <Link href="/">
+                  <Image
+                    className="h-auto w-[100px]"
+                    src="/logo.png"
+                    alt="Bridge The Gap Logo"
+                    height={65}
+                    width={150}
+                  />
+                </Link>
 
-              <ul className="mt-20 flex min-h-screen flex-col items-start gap-5">
-                {navlinks.map((navlink, i) => (
-                  <li key={i}>
-                    <Link
-                      className="border-b-primary-bridge font-semibold hover:border-b-2 hover:text-primary-bridge"
-                      href={navlink.href}
-                      onClick={closeSheet}
-                    >
-                      {navlink.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+                <ul className="mt-20 flex flex-col items-start gap-5">
+                  {navlinks.map((navlink, i) => (
+                    <li key={i}>
+                      <Link
+                        className="border-b-primary-bridge font-semibold hover:border-b-2 hover:text-primary-bridge"
+                        href={navlink.href}
+                        onClick={closeSheet}
+                      >
+                        {navlink.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+
+                <div className="mb-14 mt-auto flex items-center justify-start gap-5 text-xl">
+                  <Link
+                    className="text-black"
+                    href="https://www.facebook.com/bridgethegap25/"
+                    target="_blank"
+                  >
+                    <FaFacebookF className="cursor-pointer" />
+                  </Link>
+
+                  <Link
+                    className="text-black"
+                    href="https://x.com/BridgeTheGap25"
+                    target="_blank"
+                  >
+                    <FaXTwitter className="cursor-pointer" />
+                  </Link>
+
+                  <Link
+                    className="text-black"
+                    href="https://www.instagram.com/bridge_the_gap25/"
+                    target="_blank"
+                  >
+                    <FaInstagram className="cursor-pointer" />
+                  </Link>
+
+                  <Link
+                    className="text-black"
+                    href="https://www.pinterest.com/dizibridgethegap/"
+                    target="_blank"
+                  >
+                    <FaPinterestP className="cursor-pointer" />
+                  </Link>
+
+                  <Link
+                    className="text-black"
+                    href="https://www.youtube.com/@BridgetheGap-25"
+                    target="_blank"
+                  >
+                    <FaYoutube className="cursor-pointer" />
+                  </Link>
+
+                  <Link
+                    className="text-black"
+                    href="https://www.tiktok.com/@bridgethegap25?lang=en"
+                    target="_blank"
+                  >
+                    <FaTiktok className="cursor-pointer" />
+                  </Link>
+                </div>
+              </div>
             </SheetContent>
           </Sheet>
         </div>
