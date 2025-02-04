06833c56adbb68d5da7a71e135bcaa315a41f3d9
AbabilGroup <itek.department@gmail.com>
2025-02-04 17:38:12 +0600

    add mobile navigation drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
+"use client";
+
 import { navlinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import { FaBars } from "react-icons/fa6";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeSheet = () => setIsOpen(false);
+
   return (
     <header className="sticky top-0 z-50 bg-white py-3 shadow">
       <div className="container flex items-center justify-between">
@@ -39,18 +39,34 @@ const Header = () => {
         </ul>
 
         <div className="lg:hidden">
-          <Sheet>
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger>
               <FaBars className="cursor-pointer text-3xl" />
             </SheetTrigger>
             <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
-                <SheetDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </SheetDescription>
-              </SheetHeader>
+              <Link href="/">
+                <Image
+                  className="h-auto w-[100px]"
+                  src="/logo.png"
+                  alt="Bridge The Gap Logo"
+                  height={65}
+                  width={150}
+                />
+              </Link>
+
+              <ul className="mt-20 flex min-h-screen flex-col items-start gap-5">
+                {navlinks.map((navlink, i) => (
+                  <li key={i}>
+                    <Link
+                      className="border-b-primary-bridge font-semibold hover:border-b-2 hover:text-primary-bridge"
+                      href={navlink.href}
+                      onClick={closeSheet}
+                    >
+                      {navlink.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </SheetContent>
           </Sheet>
         </div>
