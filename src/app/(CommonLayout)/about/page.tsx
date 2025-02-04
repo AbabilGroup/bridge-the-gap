2a3235a6d0e093a2e17fe2241529c8bc2d66e828
AbabilGroup <itek.department@gmail.com>
2025-02-04 16:25:15 +0600

    remove our partners section

--- src/app/(CommonLayout)/about/page.tsx
+++ src/app/(CommonLayout)/about/page.tsx
@@ -1,6 +1,5 @@
 import AboutUsBanner from "@/components/pages/about-us/AboutUsBanner";
 import CompanyOverview from "@/components/pages/about-us/CompanyOverview";
-import OurPartners from "@/components/pages/about-us/OurPartners";
 import OurValue from "@/components/pages/about-us/OurValue";
 import Subsidiaries from "@/components/pages/about-us/Subsidiaries";
 import { Metadata } from "next";
@@ -17,7 +16,7 @@ const AboutPage = () => {
       {/* <Timeline /> */}
       <OurValue />
       <Subsidiaries />
-      <OurPartners />
+      {/* <OurPartners /> */}
     </main>
   );
 };
